e50b8eb098f4db148bf3730d4812ebdd59365223
Jakub Janšta <jansta.ja@gmail.com>
2022-08-15 19:37:14 +0200

    Revert "Update plugin.cfg"
    This reverts commit 650aa7a29929e96c20a73e9d32384ef63f1e1740.
    
    add git commit hash option
    
    Port to Godot 4
    
    Update plugin.cfg

--- addons/AutoExportVersion/AutoExportVersion.gd
+++ addons/AutoExportVersion/AutoExportVersion.gd
@@ -1,36 +1,50 @@
-tool
+@tool
 extends EditorPlugin
 
 ## Path to the version script file (bruh).
-const VERSION_SCRIPT_PATH = "res://version.gd"
+const VERSION_SCRIPT_PATH: String = "res://version.gd"
 
 ## Change the code of this method to return a String that will identify your version.
 ## Two example ways of doing so are provided, just uncomment one of them.
 ## You can use the arguments to customize your version based on selected platform or something.
-func _fetch_version(features: PoolStringArray, is_debug: bool, path: String, flags: int) -> String:
+func _fetch_version(features: PackedStringArray, is_debug: bool, path: String, flags: int) -> String:
 	### Git version ### ---------------------------------------------------------------------------
 	
 	# Version is number of commits. Requires git installed
 	# and project inside git repository with at least 1 commit.
 	
 #	var output := []
-#	OS.execute("git", PoolStringArray(["rev-list", "--count", "HEAD"]), true, output)
-#	if output.empty() or output[0].empty():
+#	OS.execute("git", PackedStringArray(["rev-list", "--count", "HEAD"]), output)
+#	if output.is_empty() or output[0].is_empty():
 #		push_error("Failed to fetch version. Make sure you have git installed and project is inside valid git directory.")
 #	else:
 #		return output[0].trim_suffix("\n")
-
+	
 	### Git branch version ### --------------------------------------------------------------------
 	
 	# Version is the current branch name. Useful for feature branches like 'release-1.0.0'
 	# Requires git installed and project inside git repository.
-
+	
 #	var output := []
-#	OS.execute("git", PoolStringArray(["rev-parse", "--abbrev-ref", "HEAD"]), true, output)
-#	if output.empty() or output[0].empty():
+#	OS.execute("git", PackedStringArray(["rev-parse", "--abbrev-ref", "HEAD"]), output)
+#	if output.is_empty() or output[0].is_empty():
 #		push_error("Failed to fetch version. Make sure you have git installed and project is inside valid git directory.")
 #	else:
 #		return output[0].trim_suffix("\n")
+	
+	### Git commit hash ### -----------------------------------------------------------------------
+	
+	# Either full or short hash of the current commit. Useful for versions like '1.0.0-[24386f9]'
+	# Requires git installed and project inside git repository.
+	
+	# Length of the hash. -1 is the whole hash (40 characters) but most of the times is is shortenet to the 7 initial characters
+#	var length := 7
+#	var output := []
+#	OS.execute("git", PackedStringArray(["rev-parse", "HEAD"]), output)
+#	if output.is_empty() or output[0].is_empty():
+#		push_error("Failed to fetch version. Make sure you have git installed and project is inside valid git directory.")
+#	else:
+#		return output[0].trim_suffix("\n").substr(0, length)
 	
 	### Profile version ### -----------------------------------------------------------------------
 	
@@ -113,18 +127,18 @@ func _enter_tree() -> void:
 	exporter = AEVExporter.new()
 	exporter.plugin = self
 	add_export_plugin(exporter)
-	add_tool_menu_item("Print Current Version", self, "print_version")
+	add_tool_menu_item("Print Current Version", print_version)
 	
 	if not File.new().file_exists(VERSION_SCRIPT_PATH):
-		exporter.store_version(_fetch_version(PoolStringArray(), true, "", 0))
+		exporter.store_version(_fetch_version(PackedStringArray(), true, "", 0))
 
 func _exit_tree() -> void:
 	remove_export_plugin(exporter)
 	remove_tool_menu_item("Print Current Version")
 
-func print_version(ud):
-	var v = _fetch_version(PoolStringArray(), true, "", 0)
-	if v.empty():
+func print_version():
+	var v = _fetch_version(PackedStringArray(), true, "", 0)
+	if v.is_empty():
 		OS.alert("Error fetching version. Check console for details.")
 	else:
 		OS.alert("Current game version: %s" % v)
@@ -133,15 +147,15 @@ func print_version(ud):
 class AEVExporter extends EditorExportPlugin:
 	var plugin
 	
-	func _export_begin(features: PoolStringArray, is_debug: bool, path: String, flags: int):
+	func _export_begin(features: PackedStringArray, is_debug: bool, path: String, flags: int):
 		var version: String = plugin._fetch_version(features, is_debug, path, flags)
-		if version.empty():
+		if version.is_empty():
 			push_error("Version string is empty. Make sure your _fetch_version() is configured properly.")
 		
 		store_version(version)
 
 	func store_version(version: String):
 		var script = GDScript.new()
-		script.source_code = str("extends Reference\nconst VERSION = \"", version, "\"\n")
-		if ResourceSaver.save(VERSION_SCRIPT_PATH, script) != OK:
+		script.source_code = str("extends RefCounted\nconst VERSION: String = \"", version, "\"\n")
+		if ResourceSaver.save(script, VERSION_SCRIPT_PATH) != OK:
 			push_error("Failed to save version file. Make sure the path is valid.")
